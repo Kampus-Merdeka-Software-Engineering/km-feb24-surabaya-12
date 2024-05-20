394c5fb3d04d95f855ab6fe71b366ab99263ddf3
aliefalmasyah <167989578+aliefalmasyah@users.noreply.github.com>
2024-05-20 20:00:49 +0700

    Update script.js
    Melakukan pengambilan data dari json file menggunakan fetch .then

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-const input = document.getElementById("DataBuilding");
-const ul = document.getElementById("tasklist");
-function submit(){
-    const li = document.createElement("li");
-    li.innerText=input.value;
-    if(input.value == ""){
-        alert("input data is required")
-    } else{
-        ul.appendChild(li);
-        li.addEventListener("click", 
-            function(){
-                ul.removeChild(li)
-            }
-        )
-        input.value=""
-        
-    }
-    
-}
+const search = document.querySelector("#show")
+search.addEventListener("click",
+    function(){
+        fetch("data.json")
+        .then ((Response => {
+            return Response.json()
+            }))
+        .then((data=>{
+            // console.log(data[0].BOROUGH)
+            let container = document.getElementById("tableee");
+            let table = document.createElement("table");
+            let cols  = Object.keys(data[0]);
+            let thead = document.createElement("thead");
+            let tr    = document.createElement("tr");
+            cols.forEach((item)=>{
+                let th = document.createElement("th");
+                th.innerText = item;
+                tr.appendChild(th); 
+                thead.appendChild(tr);
+                table.appendChild(tr);
+                })
+            data.forEach((item)=> {
+                let tr = document.createElement("tr");
+                let vals = Object.values(item);
+                vals.forEach((elem)=>{
+                      let td  = document.createElement("td");
+                      td.innerText  = elem;
+                      tr.appendChild(td);
+                    })
+                    table.appendChild(tr);
+                })
+            container.appendChild(table)
+            }))
+        }
+)
